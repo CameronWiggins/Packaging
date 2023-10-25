7511ab8441a98158b81e12d9b284dbf5ac30fa4f
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-10-25 10:21:56 -0500

    testing works

--- src/App.java
+++ src/App.java
@@ -2,7 +2,9 @@ import mypkg.Stack;
 public class App {
     public static void main(String[] args) throws Exception {
         Stack<Integer> s1 = new Stack<Integer>();
+        System.out.println(s1.isEmpty());
         s1.push(5);
+        System.out.println(s1.peek());
         System.out.println(s1.pop());
     }
 }
