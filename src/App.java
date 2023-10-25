cd61e3760d0f6bdcf3377f69fe9e1013f32c0c09
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-10-25 10:15:37 -0500

    importing and testing stack class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import mypkg.Stack;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Stack<Integer> s1 = new Stack<Integer>();
+        s1.push(5);
+        System.out.println(s1.pop());
     }
 }
